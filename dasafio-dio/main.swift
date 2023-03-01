c76f649ca2b63c9b3291936ad3189babe4bbcb35
Marcos Rangel <40068058+correiarangel@users.noreply.github.com>
2023-03-01 01:04:29 -0300

    :pencil: faet: adjusts

--- dasafio-dio/main.swift
+++ dasafio-dio/main.swift
@@ -6,29 +6,30 @@
 //
 import Foundation
 
-var fname: String? = "Stive"
-let lname: String = "Jobs"
+
+let fistname: String = "Stive"
+var lastname: String? = "Jobs"
+
 let num = (0, 1)
 
 func setName(
-  firtName: String? = "Woznic",
-  lastName: String
-) {
+  firtName: String,
+  lastName: String? = "Woznic") {
   var name: String?
   print("---------------------------------------------")
 
-  if firtName == "Woznic" {
+  if lastName == "Woznic" {
 
-    name = "Fundadores \(num.1) e \(num.0) da Appla  \(firtName!) e \(lastName) ."
+    name = "Fundadores \(num.0) e \(num.1) da Apple  \(firtName) e \(lastName!)."
     print(name!)
 
   } else {
-    name = "Fundador \(num.0) da Appla  \(firtName!) \(lastName)."
+    name = "Fundador \(num.0) da Appla  \(firtName) \(lastName!)."
     print(name!)
 
   }
 
 }
-setName(firtName: fname, lastName: lname)
+setName(firtName: fistname, lastName: lastname)
 
-setName(lastName: lname)
+setName(firtName: fistname)
