f9f1e8a2dcb48d7958a6e187005cdc7b848b2df5
Marcos Rangel <40068058+correiarangel@users.noreply.github.com>
2023-02-28 22:57:52 -0300

    :tada: faet: start

--- dasafio-dio/main.swift
+++ dasafio-dio/main.swift
@@ -4,8 +4,31 @@
 //
 //  Created by Marcos F C Rangel on 28/02/23.
 //
-
 import Foundation
 
-print("Hello, World!")
+var fname: String? = "Stive"
+let lname: String = "Jobs"
+let num = (0, 1)
 
+func setName(
+  firtName: String? = "Woznic",
+  lastName: String
+) {
+  var name: String?
+  print("---------------------------------------------")
+
+  if firtName == "Woznic" {
+
+    name = "Fundadores \(num.1) e \(num.0) da Appla  \(firtName!) e \(lastName) ."
+    print(name!)
+
+  } else {
+    name = "Fundador \(num.0) da Appla  \(firtName!) \(lastName)."
+    print(name!)
+
+  }
+
+}
+setName(firtName: fname, lastName: lname)
+
+setName(lastName: lname)
